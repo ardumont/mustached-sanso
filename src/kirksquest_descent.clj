6054034526d4b11d54cdcae376e82a34c5dc5620
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-22 10:59:25 +0200

    Implementation ok

--- src/kirksquest_descent.clj
+++ src/kirksquest_descent.clj
@@ -4,6 +4,16 @@
 ;; Auto-generated code below aims at helping you parse
 ;; the standard input according to the problem statement.
 
+(defn highest-mountain? [map-mountains]
+  "Compute the highest mountain indice."
+  (->> map-mountains
+       (reduce (fn [[i h] [j k]]
+                 (if (< h k)
+                   [j k]
+                   [i h]))
+               [-1 -1])
+       first))
+
 (defn scan-mountains! [nb-mountains]
   "Scan the mountains' structure."
   (loop [i 0 map-mountains {}]
@@ -18,7 +28,7 @@
     (let [SX (read)
           SY (read)
           mountains (scan-mountains! 8)
-          mountain-to-fire-at-current-pos (mountains SX)]
+          highest-mountain-to-fire (highest-mountain? mountains)]
       (binding [*out* *err*]
-        (println (format "mountains: %s - sx: %s - below mountain height: %s" mountains SX mountain-to-fire-at-current-pos)))
-      (println (if (zero? mountain-to-fire-at-current-pos) "HOLD" "FIRE")))))
+        (println (format "mountains: %s - sx: %s - highest mountain: %s" mountains SX highest-mountain-to-fire)))
+      (println (if (= SX highest-mountain-to-fire) "FIRE" "HOLD")))))
