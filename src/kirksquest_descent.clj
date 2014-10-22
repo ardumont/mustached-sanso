171c7d19fb53adc095813c7ce47b585973150d95
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-22 11:03:15 +0200

    Refactor to simplify implementation

--- src/kirksquest_descent.clj
+++ src/kirksquest_descent.clj
@@ -1,9 +1,6 @@
 (ns Player
   (:gen-class))
 
-;; Auto-generated code below aims at helping you parse
-;; the standard input according to the problem statement.
-
 (defn highest-mountain? [map-mountains]
   "Compute the highest mountain indice."
   (->> map-mountains
@@ -20,15 +17,14 @@
     (if (= i nb-mountains)
       map-mountains
       (let [MH (read)]
-        ;; MH: represents the height of one mountain, from 9 to 0. Mountain heights are provided from left to right.
         (recur (inc i) (assoc map-mountains i MH))))))
 
 (defn -main [& args]
   (while true
     (let [SX (read)
-          SY (read)
-          mountains (scan-mountains! 8)
-          highest-mountain-to-fire (highest-mountain? mountains)]
-      (binding [*out* *err*]
-        (println (format "mountains: %s - sx: %s - highest mountain: %s" mountains SX highest-mountain-to-fire)))
-      (println (if (= SX highest-mountain-to-fire) "FIRE" "HOLD")))))
+          SY (read)]
+      (-> (scan-mountains! 8)
+           highest-mountain?
+           (= SX)
+           (if "FIRE" "HOLD")
+           println))))
